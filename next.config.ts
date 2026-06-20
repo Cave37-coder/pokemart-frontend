89bb4d004fb5cb46497f6af0df60f17cda1d2bc6
Michael V <texcavontheroad@gmail.com>
2026-06-20 16:17:41 +0200

    Move stray backend scripts into _dump folder, remove duplicate next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.pokebulk.co.za',
-                pathname: '/cards/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'pub-77a8c30ac1fc4f4fbe1f2a7a0f15f174.r2.dev',
-                pathname: '/cards/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'tcgplayer-cdn.tcgplayer.com',
-            },
-        ],
-    },
-}
-module.exports = nextConfig
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pokebulk.co.za",
+        pathname: "/cards/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-77a8c30ac1fc4f4fbe1f2a7a0f15f174.r2.dev",
+        pathname: "/cards/**",
+      },
+      {
+        protocol: "https",
+        hostname: "tcgplayer-cdn.tcgplayer.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
